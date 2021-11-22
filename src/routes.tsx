9401769a809020b659e840f00fccebd35e3f23cc
ikhumoyun <khumoyun@inoyatov.com>
2021-11-23 01:00:10 +0500

    Initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import React, { lazy } from 'react';
+import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
+
+const HomePage = lazy(() => import('./pages/home'));
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route element={<HomePage/>} path='/' />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes;
